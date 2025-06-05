5c8c8b6bf90bc2a7fa3c250bf620c924c114f508
m.kapuscinski <kapuscinski.mateusz01@gmail.com>
2025-06-05 11:41:00 +0200

    style RadioGroup component

--- src/ui/RadioGroup.tsx
+++ src/ui/RadioGroup.tsx
@@ -14,18 +14,25 @@ function RadioGroup({
     onChange,
 }: RadioGroupProps) {
     return (
-        <div>
+        <div className="space-y-4">
             {options.map(({ value, label }) => (
-                <div key={value}>
+                <div
+                    key={value}
+                    className="flex cursor-pointer items-center gap-x-2.5"
+                >
                     <input
                         type="radio"
                         name={name}
                         id={value}
-                        value={selectedValue}
                         checked={selectedValue === value}
                         onChange={() => onChange(value)}
+                        className="peer sr-only"
                     />
-                    <label htmlFor={value}>{label}</label>
+                    <div className="flex h-5 w-5 items-center justify-center rounded-full border-2 border-stone-500 transition-all duration-300 before:h-3 before:w-3 before:rounded-full before:bg-red-600 before:opacity-0 before:transition-all before:duration-300 peer-checked:border-red-600 peer-checked:before:opacity-100"></div>
+
+                    <label htmlFor={value} className="text-sm font-medium">
+                        {label}
+                    </label>
                 </div>
             ))}
         </div>
