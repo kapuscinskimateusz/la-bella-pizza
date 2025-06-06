41a30b65805120982d9a8d8e67b0b3bf7e25ecbd
m.kapuscinski <kapuscinski.mateusz01@gmail.com>
2025-06-06 12:38:37 +0200

    add path alias for @ to src, simulate data fetching for prod

--- src/services/apiRestaurant.tsx
+++ src/services/apiRestaurant.tsx
@@ -1,11 +1,19 @@
-import type { MenuItem } from '../types/menu'
+// import type { MenuItem } from '../types/menu'
 
-const API_URL = 'http://localhost:3000'
+// const API_URL = 'http://localhost:3000'
+
+import data from '@/data/db.json'
 
 export async function getMenu() {
-    const res = await fetch(`${API_URL}/menu`)
-    if (!res.ok) throw new Error('Something went wrong with getting menu')
+    // const res = await fetch(`${API_URL}/menu`)
+    // if (!res.ok) throw new Error('Something went wrong with getting menu')
 
-    const data: MenuItem[] = await res.json()
-    return data
+    // const data: MenuItem[] = await res.json()
+    // return data
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(data.menu)
+        }, 1000)
+    })
 }
