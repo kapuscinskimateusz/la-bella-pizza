76101c089d4826b43ba01cac03b56ace08ad8c9c
m.kapuscinski <kapuscinski.mateusz01@gmail.com>
2025-06-06 10:36:36 +0200

    set select width dynamically (based on the longest label of options)

--- src/ui/Select.tsx
+++ src/ui/Select.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ChevronDown } from 'lucide-react'
 
 import type { Option } from '../types/ui'
@@ -11,6 +11,16 @@ interface SelectProps {
     onChange: (newVal: string) => void
 }
 
+const optionsClasses = {
+    base: 'absolute z-50 mt-1 overflow-hidden rounded-lg border border-stone-600 bg-stone-700 py-1',
+}
+
+const optionClasses = {
+    base: 'whitespace-nowrap px-2.5 py-1.5 text-sm transition-colors duration-300',
+    active: 'bg-red-700',
+    inactive: 'hover:bg-red-700/50',
+}
+
 function Select({
     options,
     value,
@@ -19,13 +29,21 @@ function Select({
     ...props
 }: SelectProps) {
     const [isOpen, setIsOpen] = useState(false)
+    const [width, setWidth] = useState(0)
 
     const open = () => setIsOpen(true)
     const close = () => setIsOpen(false)
 
-    const ref = useOutsideClick(close)
+    const selectRef = useOutsideClick(close)
+    const hiddenRef = useRef<HTMLDivElement | null>(null)
 
     const currentOption = options.find((option) => option.value === value)
+
+    useEffect(() => {
+        if (hiddenRef.current) {
+            setWidth(Math.ceil(hiddenRef.current.getBoundingClientRect().width))
+        }
+    }, [])
 
     function handleClick(value: string) {
         onChange(value)
@@ -33,37 +51,56 @@ function Select({
     }
 
     return (
-        <div ref={ref} className="relative w-full max-w-80" {...props}>
+        <div ref={selectRef} className="relative cursor-default" {...props}>
             <div
                 tabIndex={0}
                 onClick={open}
-                className="flex h-8 items-center justify-between rounded-lg border border-stone-600 bg-stone-700 px-2.5 text-sm outline-none transition-all duration-300 focus:border-red-600 focus:ring-2 focus:ring-red-600 md:h-9"
+                className="flex h-8 items-center rounded-lg border border-stone-600 bg-stone-700 px-2.5 text-sm outline-none transition-all duration-300 focus:border-red-600 focus:ring-2 focus:ring-red-600 md:h-9"
             >
-                <span className="truncate">
+                <div style={{ width }}>
                     {currentOption?.label ?? placeholder}
-                </span>
+                </div>
 
-                <ChevronDown
-                    size={20}
-                    className={`flex-shrink-0 ${isOpen ? 'rotate-180' : ''}`}
-                />
+                <ChevronDown size={20} className={isOpen ? 'rotate-180' : ''} />
             </div>
 
             {isOpen && (
-                <div className="absolute z-50 mt-1 w-full overflow-hidden rounded-lg border border-stone-600 bg-stone-700 py-1">
+                <div className={`${optionsClasses.base} w-full`}>
                     {options.map((option) => (
                         <div
                             key={option.value}
                             onClick={() => handleClick(option.value)}
-                            className={`cursor-default whitespace-nowrap px-2.5 py-1.5 text-sm transition-colors duration-300 ${option.value === currentOption?.value ? 'bg-red-700' : 'hover:bg-red-700/50'}`}
+                            className={[
+                                optionClasses.base,
+                                option.value === currentOption?.value
+                                    ? optionClasses.active
+                                    : optionClasses.inactive,
+                            ].join(' ')}
                         >
                             {option.label}
                         </div>
                     ))}
                 </div>
             )}
+
+            {/* HIDDEN - It's only to calculate the select width */}
+            <div ref={hiddenRef} className={`${optionsClasses.base} invisible`}>
+                <div
+                    className={`${optionClasses.base} ${optionClasses.active}`}
+                >
+                    {getWidestLabel(options)}
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Select
+
+function getWidestLabel(options: Option<string>[]) {
+    if (options.length === 0) return ''
+
+    return options.reduce((widest, option) =>
+        widest.label.length > option.label.length ? widest : option
+    ).label
+}
