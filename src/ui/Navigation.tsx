f8f1ee84ce86d11eb2442302efb3a2e5d606242b
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-06-08 21:14:05 +0200

    fix pipeline

--- src/ui/Navigation.tsx
+++ src/ui/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router'
-import { Menu, X } from 'lucide-react'
+import { Menu } from 'lucide-react'
 
 import { useOutsideClick } from '@/hooks/useOutsideClick'
 import CloseButton from './CloseButton'
